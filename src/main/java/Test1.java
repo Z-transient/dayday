410c8ee35f537ce34ee50d34ecf9caa7c0c4d7d0
zss <2076563845@qq.com>
2021-04-21 17:01:10 +0800

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * @author diandian
+ * @create 2021-04-21 17:00
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("第一次提交到github的主干");
+    }
+}

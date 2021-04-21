cc3ba239b26dae24b4ced994f4127ab87d57e0c7
zss <2076563845@qq.com>
2021-04-21 17:03:05 +0800

    第二次提交到github的分支

---
+++ src/main/java/Test2.java
@@ -0,0 +1,9 @@
+/**
+ * @author diandian
+ * @create 2021-04-21 17:01
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("第二次提交到github的分支");
+    }
+}
